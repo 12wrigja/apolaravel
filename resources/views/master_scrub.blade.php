208ef80f47a733b197fee5419963a7d27821dc18
James Wright <12wrigja@gmail.com>
2015-09-29 08:04:18 -0400

    Url updates to use URL helper.
    Signed-off-by: James Wright <12wrigja@gmail.com>

--- resources/views/master_scrub.blade.php
+++ resources/views/master_scrub.blade.php
@@ -42,7 +42,8 @@
                     @include('userAuth')
                 @else
                     <li class="dropdown pull-right">
-                        <a href="/login?redirect_url={{ Request::url() }}" style="color:#777; margin-top: 5px;"
+
+                        <a href="{!! URL::to('login?redirect_url='.Request::url())!!}" style="color:#777; margin-top: 5px;"
                            class="dropdown-toggle"><span
                                     class="glyphicon glyphicon-user"></span><b> Login</b></a>
                     </li>
