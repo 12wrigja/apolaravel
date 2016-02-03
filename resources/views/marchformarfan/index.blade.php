6db317b759e5bbf8f64f834da8d515ffb24f8bde
James Wright <12wrigja@gmail.com>
2016-02-03 01:04:39 -0500

    Cache busting image name change.

--- resources/views/marchformarfan/index.blade.php
+++ resources/views/marchformarfan/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="css/images/m4mlogo.png" alt="">
+                    <img class="img-responsive" src="css/images/m4mlogov2.png" alt="">
                     <div class="intro-text">
                         <span class="name"></span>
                         <hr class="star-light">
